0a57a260cdf71adc10ff847d1971d6b3826727ca
Daniel F. Dickinson <cshored@thecshore.com>
2020-04-28 09:25:18 -0400

    Use rollup instead of webpack for bundling JS
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- test/prepare-fuse.js
+++ test/prepare-fuse.js
@@ -1,9 +1,12 @@
 /* global module require */
 
 const process = require('process')
-if (process.env.MODE === 'dev') {
-  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.dev') // eslint-disable-line no-unused-vars
-} else {
+if (process.env.TARGET === 'umd-dev') {
   module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse') // eslint-disable-line no-unused-vars
+} else if (process.env.TARGET === 'commonjs') {
+  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.common') // eslint-disable-line no-unused-vars
+} else if (process.env.TARGET === 'esm-dev') {
+  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.esm') // eslint-disable-line no-unused-vars
+} else {
+  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.min') // eslint-disable-line no-unused-vars
 }
-
