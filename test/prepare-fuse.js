2d07028978900c42010e23807d957a6490121d52
Daniel F. Dickinson <cshored@thecshore.com>
2020-04-28 09:35:51 -0400

    Add missing extension from test module import
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- test/prepare-fuse.js
+++ test/prepare-fuse.js
@@ -2,11 +2,11 @@
 
 const process = require('process')
 if (process.env.TARGET === 'umd-dev') {
-  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse') // eslint-disable-line no-unused-vars
+  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.js') // eslint-disable-line no-unused-vars
 } else if (process.env.TARGET === 'commonjs') {
-  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.common') // eslint-disable-line no-unused-vars
+  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.common.js') // eslint-disable-line no-unused-vars
 } else if (process.env.TARGET === 'esm-dev') {
-  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.esm') // eslint-disable-line no-unused-vars
+  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.esm.js') // eslint-disable-line no-unused-vars
 } else {
-  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.min') // eslint-disable-line no-unused-vars
+  module.exports = require('../_vendor/github.com/cshoredaniel/krisk-Fuse/dist/fuse.min.js') // eslint-disable-line no-unused-vars
 }
